83c551f5f7dfa7e45ab5a07736248c9d889147ed
Vishwa-Joshi <vishwa.joshi.2023@gmail.com>
2023-04-25 18:36:37 +0530

    starting the project finance tracker..initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+// import {Link} from 'react-router-dom';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+   <h1>Finance Tracker</h1>
+    </div>
+  );
+}
+
+export default App;
